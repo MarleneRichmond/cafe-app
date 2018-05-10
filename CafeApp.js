3397b1059a88bd1bdd453acd3c28091ab8141d19
TonyByrd <39130933+TonyByrd@users.noreply.github.com>
2018-05-10 12:17:08 -0400

    First Commit
    About to get this going!

---
+++ CafeApp.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Tony Byrd on 5/10/18.
+ */
+
+var List = require('prompt-list');
+
+
+// Greeting print out
+console.log("Hi welcome to the CafeApp")
+
+
+// prompt the user for input
+
+
+var list = new List({
+    name: 'order',
+    message: 'What would you like to order?',
+    // choices may be defined as an array or a function that returns an array
+    choices: [
+        'Coke',
+        'Diet Coke',
+        'Cherry Coke',
+        {name: 'Sprite', disabled: 'Temporarily unavailable'},
+        'Water'
+    ]
+});
+
+// async
+/*
+list.ask(function(answer) {
+    console.log(answer);
+});
+*/
+
+// promise
+list.run()
+    .then(function(answer) {
+        console.log(answer);
+    });
+
+
+
+// check there input and handle error (within the range)
+
+
